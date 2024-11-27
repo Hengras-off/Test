56b0afb5fbd456f7e7925e7aea6657dd22d3e3d8
Hengras-off <vikapodgainaja400@gmail.com>
2024-11-28 01:33:09 +0200

    index.html

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://kinobox.tv/kinobox.min.js"></script>
+</head>
+<body>
+    <div class="kinobox_player" style="width: 700px;"></div>
+    <script>kbox('.kinobox_player', {search: {kinopoisk: '1234567'}})</script>
+</body>
+</html>
